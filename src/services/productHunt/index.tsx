7ec0ae57823f609cfae20f20c61e7e6cac8aaca9
Pedro Araujo <pedro-foa@hotmail.com>
2021-02-18 00:30:53 -0300

    :wrench: Configure eslint and api

---
+++ src/services/productHunt/index.tsx
@@ -0,0 +1,22 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const PRODUCT_HUNT_API = 'https://api.producthunt.com/v2/api/graphql';
+
+const httpLink = createHttpLink({
+  uri: PRODUCT_HUNT_API,
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
---
+++ src/services/productHunt/queries/post.ts
@@ -0,0 +1,32 @@
+import { gql } from '@apollo/client';
+
+export const GET_POSTS = gql`
+  query getPosts($order: PostsOrder!) {
+    posts(order: $order) {
+      edges {
+        node {
+          id
+          name
+          votesCount
+          description
+          thumbnail {
+            url
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_POST = gql`
+  query getPost($id: ID) {
+    post(id: $id) {
+      name
+      description
+      thumbnail {
+        url
+      }
+      votesCount
+    }
+  }
+`;
