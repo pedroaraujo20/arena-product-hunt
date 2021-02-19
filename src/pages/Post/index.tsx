4300bd968214c1fa90ab0c546adccc0607681d2a
Pedro Araujo <pedro-foa@hotmail.com>
2021-02-18 23:19:33 -0300

    :construction: Started Post screen

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import Loading from '../../components/Loading';
+import { useGetPost } from '../../hooks';
+import { useQuery } from '../../utils/useQuery';
 
 // import { Container } from './styles';
 
 const Post: React.FC = () => {
-  return <h1>Post</h1>;
+  const query = useQuery();
+
+  const { post, loading } = useGetPost(query.get('id') as string);
+
+  const renderPost = useMemo(() => {
+    if (loading) return <Loading />;
+
+    return (
+      <div>
+        <p>{post.name}</p>
+        <p>{post.description}</p>
+        <p>{post.votesCount}</p>
+        <p>{post.website}</p>
+        <img src={post.thumbnail.url} alt={post.name} />
+      </div>
+    );
+  }, [loading, post]);
+
+  return <>{renderPost}</>;
 };
 
 export default Post;
